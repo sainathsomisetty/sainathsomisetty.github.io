b88239bd5c62393af8351406212bc6b7f61a7561
sainathsomisetty <s.sainathogl@gmail.com>
2019-07-19 08:09:24 +0530

    edited wsgi file removed whitenoise

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,9 +10,6 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
